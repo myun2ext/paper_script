0328e5285bae8915c6a749c971a9f136c6c2e074
myun2 <myun2@nwhite.info>
2015-07-09 16:10:19 +0900

    implement print module

--- paper.cpp
+++ paper.cpp
@@ -1,4 +1,5 @@
 #include "paper.hpp"
+#include "paper_script/modules/print.hpp"
 using namespace myun2;
 
 int main()
--- paper_script/modules/print.hpp
+++ paper_script/modules/print.hpp
@@ -3,14 +3,24 @@
 
 namespace myun2
 {
-	class paper_script
+	namespace paper_script_builtin_modules
 	{
-		struct modules {
-			struct print {
-				print(const tokens&
-			};
+		typedef typename paper_script::token_list token_list, tokens_type;
+		typedef typename paper_script::token_iterator token_iterator;
+
+		struct print {
+			token_iterator last;
+			print(token_iterator it, const token_iterator end)
+			{
+				const ::std::string token = *it;
+				while ( token != "." && it != end )
+				{
+					if ( token != "\"" )
+						puts(token.c_str());
+				}
+			}
 		};
-	};
+	}
 }
 
 #endif///__MYUN2_GITHUB_COM__PAPER_SCRIPT__MODULES__PRINT_HPP__
